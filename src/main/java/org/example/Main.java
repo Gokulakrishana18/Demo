880c9ba8f0e751f9242df3ee2132eff5501e3ea0
Gokulakrishana18 <gokulrajender@gmail.com>
2023-06-12 15:31:58 +0530

    chnageing the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,68 @@
 package org.example;
-import okhttp3.*;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
+        HttpClient client = HttpClients.createDefault();
 
         // Replace with your GitHub access token
-        String accessToken = "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";
+        String accessToken = "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";
 
         // Replace with the repository owner, repository name, and base branch
-        String owner = "YOUR_REPOSITORY_OWNER";
-        String repo = "YOUR_REPOSITORY_NAME";
-        String baseBranch = "master";
+        String owner = "Gokulakrishana18";
+        String repo = "Demo";
+        String baseBranch = "424428026269aaed4966681759ae43a531719c4b";
 
         // Replace with the name of the new branch you want to create
-        String newBranchName = "new-branch";
+        String newBranchName = "DCP-666";
 
-        // Prepare the API request
-        Request request = new Request.Builder()
-                .url("https://api.github.com/repos/" + owner + "/" + repo + "/git/refs")
-                .header("Authorization", "Bearer " + accessToken)
-                .post(RequestBody.create(MediaType.parse("application/json"),
-                        "{\"ref\":\"refs/heads/" + newBranchName + "\",\"sha\":\"" + baseBranch + "\"}"))
-                .build();
+        // Prepare the API endpoint URL
+        String url = "https://api.github.com/repos/" + owner + "/" + repo + "/git/refs";
+
+        // Prepare the request payload
+        String payload = "{\"ref\":\"refs/heads/" + newBranchName + "\",\"sha\":\"" + baseBranch + "\"}";
+
+        // Prepare the HTTP request
+        HttpPost request = new HttpPost(url);
+        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+        request.setEntity(new StringEntity(payload, ContentType.APPLICATION_JSON));
 
         try {
-            // Send the API request
-            Response response = client.newCall(request).execute();
+            // Send the HTTP request
+            HttpResponse response = client.execute(request);
 
-            if (response.isSuccessful()) {
+            // Read the response
+            HttpEntity responseEntity = response.getEntity();
+            String responseBody = EntityUtils.toString(responseEntity);
+
+            if (response.getStatusLine().getStatusCode() == 201) {
                 System.out.println("Branch created successfully.");
             } else {
-                System.out.println("Failed to create branch. Error: " + response.code() + " " + response.message());
+                System.out.println("Failed to create branch. Error: " + response.getStatusLine().getStatusCode() + " " + responseBody);
             }
 
-            // Close the response
-            response.close();
-        } catch (Exception e) {
+            // Consume the response entity to release resources
+            EntityUtils.consume(responseEntity);
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
 
 
 
